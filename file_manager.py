2694874890f3cbcdf304a3bee2f4ae32237486f7
yana <yanchik_vau@mail.ru>
2020-09-02 10:41:21 +0300

    add logging

--- file_manager.py
+++ file_manager.py
@@ -8,24 +8,9 @@ def get_free_memory_gb():
     return used // (2 ** 30)
 
 
-def check_directory(dir):
-    ret = True
-    if not path.isdir(dir):
-        try:
-            os.mkdir(dir)
-        except OSError as e:
-            ret = False
-            print("Creation of the directory %s failed" % dir)
-            print(e)
-        else:
-            print("Successfully created the directory %s " % dir)
-    else:
-        print(dir + " exist")
-    return ret
-
-
 class FileManager:
-    def __init__(self, min_mem, rm_koef):
+    def __init__(self, logger, min_mem, rm_koef):
+        self.logger = logger
         self.min_memory = min_mem
         self.rm_koef = rm_koef
         self.directories = ['/home/pi/data', '/home/pi/data1']
@@ -33,17 +18,15 @@ class FileManager:
 
     def memory_enough(self):
         total, used, free = shutil.disk_usage("/")
-        print("Total: %d GiB" % (total // (2 ** 30)))
-        print("Used: %d GiB" % (used // (2 ** 30)))
-        print("Free: %d GiB" % (free // (2 ** 30)))
-
+        self.logger.info("Total: %d GiB" % (total // (2 ** 30)))
+        self.logger.info("Used: %d GiB" % (used // (2 ** 30)))
         if free // (2 ** 30) < self.min_memory:
             return False
         else:
             return True
 
     def clear_memory(self):
-        print("memory clearing...")
+        self.logger.info("memory clearing...")
         for i in range(100):
             for dir in self.directories:
                 for file in self.file_template:
@@ -52,24 +35,39 @@ class FileManager:
                         try:
                             os.remove(file_path)
                         except Exception as e:
-                            print(e)
+                            self.logger.exception(e)
                             return
             if get_free_memory_gb() > self.rm_koef * self.min_memory:
                 break
 
+    def check_directory(self, dir):
+        ret = True
+        if not path.isdir(dir):
+            try:
+                os.mkdir(dir)
+            except OSError as e:
+                ret = False
+                self.logger.error("Creation of the directory %s failed" % dir)
+                self.logger.exception(e)
+            else:
+                self.logger.info("Successfully created the directory %s " % dir)
+        else:
+            self.logger.info(dir + " exist")
+        return ret
+
     def check_folders(self):
         for directory in self.directories:
-            if not check_directory(directory):
-                print("fail open directory " + directory)
+            if not self.check_directory(directory):
+                self.logger.error("fail open directory " + directory)
                 return False
         return True
 
     def check_memory(self):
         if not self.memory_enough():
-            print("not enough free space")
+            self.logger.warning("not enough free space")
             self.clear_memory()
             if not self.memory_enough():
-                print("can not remove enough files")
+                self.logger.error("can not remove enough files")
                 return False
         return True
 
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
-import shutil
+import logging
+import logging.handlers
 
 from file_manager import FileManager
 
 CONFIG_FILE = 'config.json'
+LOG_FILE_NAME = 'RPiDevLogger.log'
 
 FREQ = 0
 MIN_MEM_FREE = 1
@@ -12,17 +14,16 @@ RM_KOEF = 2
 keys = ['frequency', 'min_free_space_gb', 'file_remove_koef']
 
 
-def memory_enough():
-    total, used, free = shutil.disk_usage("/")
+logger = logging.getLogger("RPiDevLogger")
 
-    print("Total: %d GiB" % (total // (2 ** 30)))
-    print("Used: %d GiB" % (used // (2 ** 30)))
-    print("Free: %d GiB" % (free // (2 ** 30)))
 
-    if free < 1:
-        return False
-    else:
-        return True
+def set_logger():
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler = logging.handlers.RotatingFileHandler(LOG_FILE_NAME, maxBytes=20480, backupCount=10)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    # logger.setLevel(logging.ERROR)
+    logger.setLevel(logging.INFO)
 
 
 def read_config():
@@ -31,22 +32,22 @@ def read_config():
         with open(CONFIG_FILE) as f:
             j_dict = json.load(f)
     except Exception as e:
-        print(e)
+        logger.exception(e)
     return j_dict
 
 
 def format_config(j_dict):
     params = {}
     if j_dict is None:
-        print("fail to read configuration file")
+        logger.error("fail to read configuration file")
         return None
     for key in keys:
         if key not in j_dict:
-            print('key "{}" missed in configuration file'.format(key))
+            logger.error('key "{}" missed in configuration file'.format(key))
             return None
         val = validate_value(j_dict[key])
         if val is None:
-            print('{} value couldn\'t be converted to float'.format(key))
+            logger.error('{} value couldn\'t be converted to float'.format(key))
             return None
         params[key] = val
     return params
@@ -57,18 +58,20 @@ def validate_value(val):
     try:
         ret = float(val)
     except Exception as e:
-        print(e)
+        logger.exception(e)
     finally:
         return ret
 
 
 def main():
+    set_logger()
+    logger.info("******** START ***********")
     config = format_config(read_config())
     if config is None:
         return
-    print("configuration: {}".format(config))
+    logger.info("configuration: {}".format(config))
 
-    fm = FileManager(config[keys[MIN_MEM_FREE]], config[keys[RM_KOEF]])
+    fm = FileManager(logger, config[keys[MIN_MEM_FREE]], config[keys[RM_KOEF]])
     if not fm.check_folders():
         return
 
